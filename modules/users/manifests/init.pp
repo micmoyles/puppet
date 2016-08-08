36d7317a693e783851b065949a8c8e9904529eec
Michael Moyles <micmoyles@gmail.com>
2016-08-08 14:49:20 +0100

    add erova users

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -13,23 +13,14 @@ class users {
         	shell => '/bin/bash',
         	groups => ['svn','puppet','sudo','erova']
 	    }
-	file { '/home/mmoyles/.ssh/amazon_linux.pem':
-		ensure => file,
-		owner => mmoyles,
-		group => mmoyles,
-		mode  => 0600,
-		source => 'puppet:///modules/users/amazon_linux.pem';
-	}
+#	file { '/home/mmoyles/.ssh/amazon_linux.pem':
+#		ensure => file,
+#		owner => mmoyles,
+#		group => mmoyles,
+#		mode  => 0600,
+#		source => 'puppet:///modules/users/amazon_linux.pem';
+#	}
 
-# passwd is erova123
-        user { 'erova':
-   		home => '/home/erova',
-        	ensure => present,
-		password => '$1$JaQmbDDv$4XzeggNopjsO4507Vq0uT/',
-        	comment => 'Erova, shared user',
-        	shell => '/bin/bash',
-        	groups => ['svn','erova']
-   	 }
 	}
 
 # best to try using managehome => true but this actually doesn't seem to work
@@ -46,3 +37,31 @@ class users::pi inherits users {
         groups => ['svn','puppet','sudo','erova']
     }
 }
+class users::erova inherits users {
+
+        user { 'erova':
+   		home => '/home/erova',
+        	ensure => present,
+		password => '$1$JaQmbDDv$4XzeggNopjsO4507Vq0uT/', #erova123
+        	comment => 'Erova, shared user',
+        	shell => '/bin/bash',
+        	groups => ['svn','erova']
+   	 }
+        user { 'mgolden':
+   		home => '/home/mgolden',
+        	ensure => present,
+		password => '$1$IggdB0.D$f8PMmLrohZwsVBCKSqx9V1', #gmatt1
+        	comment => 'Matt Golden',
+        	shell => '/bin/bash',
+        	groups => ['svn','erova','sudo']
+   	 }
+        user { 'mbrennan':
+   		home => '/home/mbrennan',
+        	ensure => present,
+		password => '$1$uPryyIuC$VE2MWYR9t7MQFLCyNUq/B1',#bmich1
+        	comment => 'Michael Brennan'
+        	shell => '/bin/bash',
+        	groups => ['svn','erova','sudo']
+   	 }
+}
+
