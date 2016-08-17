33df354ecc2dcdbc7ed0e96ede9cb8dd35cbf9ce
root <root@ip-172-31-45-132.eu-west-1.compute.internal>
2016-08-17 18:06:42 +0000

    move directoies to user class

---
+++ modules/users/manifests/directories.pp
@@ -0,0 +1,29 @@
+class directories::home {
+	file { '/home/mmoyles':
+		owner => mmoyles, group => mmoyles, mode => 0755,
+		ensure => "directory",
+	}
+	file { '/home/mmoyles/.ssh':
+		owner => mmoyles, group => mmoyles, mode => 0755,
+		ensure => "directory",
+	}
+	file { '/home/erova':
+		owner => erova, group => erova, mode => 0755,
+		ensure => "directory",
+	}
+	file { '/home/mbrennan':
+		owner => mbrennan, group => mbrennan, mode => 0755,
+		ensure => "directory",
+	}
+	file { '/home/mgolden':
+		owner => mgolden, group => mgolden, mode => 0755,
+		ensure => "directory",
+	}
+}
+class directories::home::pi inherits directories::home {
+	file { '/home/michael':
+		owner => michael, group => michael, mode => 0755,
+		ensure => "directory",
+	}
+
+}
