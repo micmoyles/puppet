75efb321b5699da3575d99628e03f6cc5ac3d0a4
root <root@ip-172-31-45-132.eu-west-1.compute.internal>
2016-08-17 18:06:19 +0000

    update erova aliases. Add public key to user class

---
+++ modules/erova-boot/files/erova_aliases.sh
@@ -0,0 +1,5 @@
+alias goev='cd /home/erova'
+alias gorun='cd /home/erova/runway'
+alias puprun='sudo puppet agent -t'
+alias puptest='sudo puppet agent -t --verbose --noop'
+alias tlogs='tail -F /var/log/syslog'
