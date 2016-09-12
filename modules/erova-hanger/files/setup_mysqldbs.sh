34243079b6b5f0afca44123328f9ac40604d31ab
root <root@ip-172-31-45-132.eu-west-1.compute.internal>
2016-09-12 14:31:18 +0000

    more tables

--- modules/erova-hanger/files/setup_mysqldbs.sh
+++ modules/erova-hanger/files/setup_mysqldbs.sh
@@ -19,6 +19,7 @@ mysql -uroot -e "create table if not exists REMIT.frequency ( timestamp timestam
 mysql -uroot -e "create table if not exists REMIT.SOSO ( pubTs timestamp, PT float(10,5) , TD varchar(3), IC varchar(30), ST timestamp, TT varchar(30), TQ int(100) ,unique(pubTs) )"
 mysql -uroot -e "create table if not exists REMIT.outages (messageCreationTs timestamp, AffectedUnitEIC varchar(30), AssetType varchar(30), AffectedUnit varchar(30), DurationUncertainty varchar(30), RelatedInformation varchar(200), AssetId varchar(30), EventType varchar(30), NormalCapacity float(10,5), AvailableCapacity float(10,5), EventStatus varchar(30), EventStart timestamp, EventEnd timestamp, Cause varchar(30), FuelType varchar(30), Participant_MarketParticipantID varchar(30), MessageHeading varchar(30) )"
 mysql -uroot -e "create table if not exists config.plants ( name varchar(30), AssetID varchar(30), FuelType varchar(30), NormalCapacity float(10,5), plantID int not null auto_increment, primary key (plantID) )"
+mysql -uroot -e "create table if not exists trades.trades ( instrument varchar(30), InstrumentBegin timestamp , FuelType varchar(30), NormalCapacity float(10,5), plantID int not null auto_increment, primary key (plantID) )"
 mysql -uroot -e "create table if not exists position.irl ( timestamp timestamp, position float(10,5) )"
 mysql -uroot -e "create table if not exists position.uk ( timestamp timestamp, position float(10,5) )"
 mysql -uroot -e "create table if not exists position.nl ( timestamp timestamp, position float(10,5) )"
