d2ad42f1a0f60ecc9943d172c2ef9121281a8482
Michael Moyles <micmoyles@gmail.com>
2018-09-11 19:11:16 +0100

    remove dupe declaration

--- modules/basefiles/manifests/init.pp
+++ modules/basefiles/manifests/init.pp
@@ -6,13 +6,6 @@ class basefiles {
 		mode  => 0755,
 		source => 'puppet:///modules/basefiles/test_bashrc';
 	}
-		file { '/etc/vim/vimrc.local':
-		ensure => file,
-		owner => root,
-		group => root,
-		mode  => 0755,
-		source => 'puppet:///modules/basefiles/vimrc.local';
-	}
 		file { '/usr/local/bin/chelsea.sh':
 		ensure => file,
 		owner => root,
