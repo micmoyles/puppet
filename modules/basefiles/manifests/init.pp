401b3ec1fc4e88a2bdae33c87832873a37b99e71
Michael Moyles <micmoyles@gmail.com>
2018-09-26 19:15:39 +0100

    remove chelsea

--- modules/basefiles/manifests/init.pp
+++ modules/basefiles/manifests/init.pp
@@ -6,11 +6,5 @@ class basefiles {
 		mode  => 0755,
 		source => 'puppet:///modules/basefiles/test_bashrc';
 	}
-		file { '/usr/local/bin/chelsea.sh':
-		ensure => file,
-		owner => root,
-		group => root,
-		mode  => 0755,
-		source => 'puppet:///modules/basefiles/chelsea.sh';
-	}
+
 	}
