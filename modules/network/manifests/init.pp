1e7060db6086301b0d131736641801ca0028d9cd
Michael Moyles <micmoyles@gmail.com>
2018-10-03 00:48:22 +0100

    remove actions from class

--- modules/network/manifests/init.pp
+++ modules/network/manifests/init.pp
@@ -1,11 +1,5 @@
 class network::base {
-		file { '/etc/test_bashrc':
-		ensure => file,
-		owner => root,
-		group => root,
-		mode  => 0755,
-		source => 'puppet:///modules/basefiles/test_bashrc';
-	}
+
 	}
 
 class network::interfaces inherits network {
