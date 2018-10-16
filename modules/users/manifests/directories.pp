97e4584beb19f6c5f2c346eb6cae32dcc6059748
Michael Moyles <micmoyles@gmail.com>
2018-10-16 14:55:46 +0100

    remove unused class

--- modules/users/manifests/directories.pp
+++ modules/users/manifests/directories.pp
@@ -9,24 +9,6 @@ class directories::home {
 	}
 }
 
-class directories::home::erova inherits directories::home {
-	file { '/home/erova':
-		owner => erova, group => erova, mode => 0755,
-		ensure => "directory",
-	}
-	file { '/home/mbrennan':
-		owner => mbrennan, group => mbrennan, mode => 0755,
-		ensure => "directory",
-	}
-	file { '/home/mgolden':
-		owner => mgolden, group => mgolden, mode => 0755,
-		ensure => "directory",
-	}
-	file { '/home/pnash':
-		owner => pnash, group => pnash, mode => 0755,
-		ensure => "directory",
-	}
-}
 class directories::home::pi inherits directories::home {
 	file { '/home/michael':
 		owner => michael, group => michael, mode => 0755,
